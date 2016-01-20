330167955e9200aa806c35112db36945f18d834c
Maksym Baziuk <maksym_baziuk@epam.com>
2016-01-20 18:25:06 +0200

    Added cheche invalidation for Repository, service.Cache, droplet.Cache

--- dynadmin_console.js
+++ dynadmin_console.js
@@ -1,16 +1,73 @@
 // Console object name
 var EXT_CONSOLE_DIV = "DynAdminExtentionConsole";
 var EXT_CONSOLE_DIV_ID = "#" + EXT_CONSOLE_DIV;
+var EXT_CONSOLE_STATUS_DIV = 'custom_status_div';
+var EXT_CONSOLE_STATUS_DIV_ID = '#' + EXT_CONSOLE_STATUS_DIV;
 var BUTTON_STYLE = 'margin:2px;padding:2px;';
 var BUTTON_BORDER_RED = 'border: 2px solid red;';
 var BUTTON_BORDER_GREEN = 'border: 2px solid green;';
 
 initExtentionConsole();
-addChangePropertyValueButton("loggingDebug", "loggingDebug");
-addChangePropertyValueButton("loggingError", "loggingError");
-addChangePropertyValueButton("loggingInfo", "loggingInfo");
-addChangePropertyValueButton("loggingTrace", "loggingTrace");
-addChangePropertyValueButton("loggingWarning", "loggingWarning");
+fillConsole();
+
+function clearStatusDiv(name){
+	$('div #custom_' + name).html('');
+}
+
+function fillConsole(){
+	addChangePropertyValueButton("loggingDebug", "loggingDebug");
+	addChangePropertyValueButton("loggingError", "loggingError");
+	addChangePropertyValueButton("loggingInfo", "loggingInfo");
+	addChangePropertyValueButton("loggingTrace", "loggingTrace");
+	addChangePropertyValueButton("loggingWarning", "loggingWarning");
+	addCacheInvalidationButton('atg.adapter.gsa.GSARepository', 'invalidateCaches');
+	addCacheInvalidationButton('atg.service.cache.Cache', 'flush');
+	addCacheInvalidationButton('atg.droplet.Cache', 'flushCache');
+	$(EXT_CONSOLE_DIV_ID).append('<b id="' + EXT_CONSOLE_STATUS_DIV + '"></b>');
+}
+
+function addCacheInvalidationButton(className, methodName){
+	var className = $('a:contains("' + className + '")');
+	if (className.length > 0){
+		$(EXT_CONSOLE_DIV_ID).append('<br>');
+		$(EXT_CONSOLE_DIV_ID).append('Caches:');
+		var invalCacheButName = 'Invalidate';
+		var successFunc = function(){
+			//console.log('Success');
+			var statusDiv = $(EXT_CONSOLE_STATUS_DIV_ID);
+			statusDiv.html('OK');
+			setTimeout(function (){statusDiv.html('');}, 10000);
+		};
+		var errorFunc = function(){
+			//console.log('Error');
+			var statusDiv = $(EXT_CONSOLE_STATUS_DIV_ID);
+			statusDiv.html('ERROR');
+			setTimeout(function (){statusDiv.html('');}, 10000);
+		};
+		var startFunction = function(){
+			//console.log('Start');
+			var statusDiv = $(EXT_CONSOLE_STATUS_DIV_ID);
+			statusDiv.html('Pending...');
+		}
+		addMethodInvocationButton(invalCacheButName, methodName, successFunc, errorFunc, startFunction);
+	}
+}
+
+function addMethodInvocationButton(buttonName, methodName, successFunc, errorFunc, startFunction){
+	var button = createButton(buttonName, function(){
+			startFunction();
+			$.ajax({
+				type: 'POST',
+				url: window.location.href,
+				data: {'invokeMethod':methodName,'submit':'Invoke Method'},
+				success: successFunc,
+				error: errorFunc
+			})
+		}
+	);
+	button.setAttribute('style', 'margin:2px;');
+	$(EXT_CONSOLE_DIV_ID).append(button);
+}
 
 function initExtentionConsole(){
 	var hrTag = document.getElementsByTagName("hr")[0];
@@ -24,7 +81,7 @@ function initExtentionConsole(){
 //TODO for now it forks only for boolean properties
 function addChangePropertyValueButton(buttonName, propertyName){
 	var button = createButton(buttonName, function(){
-			var propertyObject = $('a:contains("' + propertyName + '")').closest('td').next().find('span');
+			var propertyObject = getPropertyValueObject(propertyName);
 			var currentValue = $.parseJSON(propertyObject.text());
 			$.ajax({
 				type: 'POST',
@@ -39,6 +96,10 @@ function addChangePropertyValueButton(buttonName, propertyName){
 	);
 	button.setAttribute('style', getButtonStyleOfBooleanTrigger(getBooleanPropertyValue(propertyName)));
 	$(EXT_CONSOLE_DIV_ID).append(button);
+}
+
+function getPropertyValueObject(propertyName){
+	return $('a:contains("' + propertyName + '")').closest('td').next().find('span');
 }
 
 function getButtonStyleOfBooleanTrigger(value){
